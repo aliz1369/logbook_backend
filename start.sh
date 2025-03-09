00c258fe17562c5e9e4102f991c911bcf7ee70c0
aliz1369 <alirezazargarian@gmail.com>
2025-03-09 17:39:10 +0300

    update bash

--- start.sh
+++ start.sh
@@ -5,7 +5,6 @@ echo "Applying database migrations..."
 python manage.py makemigrations
 python manage.py migrate
 
-
 # Start the Django server
 echo "Starting Django application..."
 gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT
